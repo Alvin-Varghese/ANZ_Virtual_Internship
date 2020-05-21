ebddec1486d973a377c895ee12e575b4b908e24b
Alvin-Varghese <biju.alv@gmail.com>
2020-05-22 02:54:24 +0530

    Update task1.R

--- task1.R
+++ task1.R
@@ -2,8 +2,11 @@ library(readxl)
 library(tidyverse)
 library(lubridate)
 library(funModeling)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
-df = read_xlsx("ANZ synthesised transaction dataset.xlsx", col_names = TRUE)
+df = read_xlsx("ANZ/ANZ synthesised transaction dataset.xlsx", col_names = TRUE)
 df_status(df)
 df$bpay_biller_code = as.character(df$bpay_biller_code)
 plot_num(df)
@@ -27,15 +30,15 @@ hist(df_day$da_avg_vol, xlab= 'Daily transaction volume', ylab='No. of customers
 
 #AVERAGE TRANSACTION BY WEEKDAY
 df_wk <- df%>% select(date, weekday) %>%
- group_by(date, weekday) %>%
- summarise(daily_avg_vol =n()) %>%
- group_by(weekday) %>%
- summarise(avg_vol=mean(daily_avg_vol,na.rm=TRUE ))
+  group_by(date, weekday) %>%
+  summarise(daily_avg_vol =n()) %>%
+  group_by(weekday) %>%
+  summarise(avg_vol=mean(daily_avg_vol,na.rm=TRUE ))
 df_wk <- df_wk %>% mutate(weekday =  factor(weekday, levels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))) %>% arrange(weekday)
- 
+
 ggplot(df_wk,aes(x=weekday, y=avg_vol)) +geom_point(colour = "#CC0000")+geom_line(aes(group = 1), colour = "Steelblue") + 
- ggtitle('Average transaction volume by weekday') +
- labs(x='Weekday',y='Transaction volume')
+  ggtitle('Average transaction volume by weekday') +
+  labs(x='Weekday',y='Transaction volume')
 
 #AVERAGE TRANSACTION PLOTTED BY HOURS
 df_hr <- df %>%
@@ -51,8 +54,9 @@ ggplot(df_hr,aes(x=hour,y=trans_vol_per_hr))+geom_point(colour ="Red")+geom_line
 tapply(df$amount, df$movement, summary)
 
 ggplot(data = df, aes(x=movement, y=amount)) +
- geom_point(aes(color=movement), alpha=0.2, position ="jitter") +
- geom_boxplot(outlier.size=4, outlier.colour='blue', alpha=0.1)
+  geom_point(aes(color=movement), alpha=0.2, position ="jitter") +
+  geom_boxplot(outlier.size=4, outlier.colour='blue', alpha=0.1)
+
 
 #Extracting Latitude and Longitudes
 #1. Customer location
@@ -74,13 +78,16 @@ df_loc$merc_long = as.numeric(df_loc$merc_long)
 #PLOTTING THE CUSTOMER AND THE MERCHANT
 merc_map <- function (id ){
   
-  plot(getMap(resolution = "low"), xlim = c(113, 154), ylim = c(-44, -10), asp = 1)
-  
   l = subset (df_loc[,c("customer_id","merc_long","merc_lat")], customer_id == id)
   l <- l[c("merc_long","merc_lat")]
-  points(as.numeric(l$merc_long), as.numeric(l$merc_lat), col ="Steelblue", cex = .5)
   
   k = subset (df_loc[,c("customer_id","cust_long","cust_lat")], customer_id == id)
   k <- unique(k[c("cust_long","cust_lat")])
-  points(as.numeric(k$cust_long), as.numeric(k$cust_lat), col ="#FF0000", cex = 1)
+  
+  ggplot(data = ne_countries(scale = "medium", returnclass = "sf")) +
+    geom_sf(fill = "#5a5a5a") +
+    coord_sf(xlim = c(113,154), ylim = c(-44,-10), expand = FALSE) +
+    geom_point(data = l, aes(x = merc_long, y = merc_lat), size = 1, shape = 5, colour ="steelblue" ) +
+    geom_point(data = k, aes(x = cust_long, y = cust_lat), size = 1, shape = 23, fill = "red") + xlab("Longitude") + ylab("Latitude")
 }
+merc_map(id = "CUS-2487424745")
